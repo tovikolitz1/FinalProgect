0d7f6be04a030efd6f63d2b8209bf8bdd8537f45
tfkolitz <tfkolitz@gmail.com>
2020-01-26 00:16:57 +0200

    api

---
+++ Web_Api/BL/ModelDTO/CardDTO.cs
@@ -0,0 +1,21 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.ModelDTO
+{
+    class CardDTO
+    {
+        public int id { get; set; }
+        public string describe { get; set; }
+        public Nullable<double> freeMonthly { get; set; }
+        public Nullable<double> freeWeekly { get; set; }
+        public Nullable<double> freeDaily { get; set; }
+        public double single { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<TransitTbl> TransitTbls { get; set; }
+    }
+}
---
+++ Web_Api/BL/UsersLogic.cs
@@ -0,0 +1,49 @@
+﻿using BL.Extentions;
+using BL.ModelDTO;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public static class UsersLogic
+    {
+        static RavKav db = new RavKav();
+
+        public static bool AddUser(UserDTO user)
+        {
+            UserTbl u = Extend.CreateUserTbl(user);
+            try
+            {
+                using (RavKav r = new RavKav())
+                {
+                    r.UserTbls.Add(u);
+                    r.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public static int CalculateThePayment(string id)
+        {
+            return 1;
+        }
+        public static int IfExsistRavKav(string ravKav, string pass)
+        {
+            if (db.UserTbls.Any(a => a.ravkav.Equals(ravKav) && a.password.Equals(pass)))
+                return db.UserTbls.Where(a => a.ravkav.Equals(ravKav) && a.password.Equals(pass)).Select(a => a.id).FirstOrDefault();
+            return 0;
+        }
+        public static string GetNameById(int id)
+        {
+             return db.UserTbls.Where(x=> x.id==id).Select(x=> x.firstName+" "+x.lastName).ToString();
+        }
+    }
+}
